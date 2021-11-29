079da3ae89c80dd5dbd3a95083b03a0333700146
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-11-29 22:54:50 +0600

    style hide

--- resources/views/layouts/backend/partials/foot.blade.php
+++ resources/views/layouts/backend/partials/foot.blade.php
@@ -3,5 +3,6 @@
 <script src="assets/backend/js/custom.min.js"></script>
 <script src="assets/backend/js/deznav-init.js"></script>
 <script src="assets/backend/js/demo.js"></script>
-<script src="assets/backend/js/styleSwitcher.js"></script>
+{{-- <script src="assets/backend/js/styleSwitcher.js"></script> --}}
+
 
