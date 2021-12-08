effeb90109075613c9012a66fc69de43fe2024fe
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2021-12-08 18:00:02 +0600

    static option

--- resources/views/livewire/backend/setting.blade.php
+++ resources/views/livewire/backend/setting.blade.php
@@ -1,3 +1,29 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <x-slot name="header">
+        <div class="sub-header">
+            <div class="d-flex align-items-center flex-wrap mr-auto">
+                <h5 class="dashboard_bar">Setting</h5>
+            </div>
+            <div class="d-flex align-items-center">
+                <a href="{{ route('dashboard') }}" class="btn btn-xs btn-primary light logout-btn">Dashboard</a>
+            </div>
+        </div>
+    </x-slot>
+
+    <div class="content-body">
+        <div class="container-fluid">
+            <div class="row page-titles mx-0">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <h4 class="card-title">{{ __('Setting') }}</h4>
+                        </div>
+                        <div class="card-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
